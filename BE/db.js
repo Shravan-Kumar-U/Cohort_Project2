aeca4ab953eca8ba1392ef74e317333988f0db4f
Shravan Kumar U <kumarshravan12546@gmail.com>
2026-02-28 16:13:48 +0530

    Started Cohort Project 2

---
+++ BE/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+mongoose.connect("mongodb://localhost:27017/paytm");
+
+const userSchema = new Schema({
+        email: {type: String, unique: true},
+        password: String,
+        firstName: String,
+        lastName: String
+})
