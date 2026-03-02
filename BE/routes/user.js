c0fefb4b6c1d4acea4c9899e3f25a0d26ff4ef8f
Shravan Kumar U <kumarshravan12546@gmail.com>
2026-03-02 18:39:41 +0530

    updated the user.js file added the signin, update, bulk routs

--- BE/routes/user.js
+++ BE/routes/user.js
@@ -3,6 +3,7 @@ const zod = require("zod");
 const jwt = require("jsonwebtoken");
 const { User } = require("../db");
 const { JWT_SECRETE } = require("../config");
+const { authMiddleware } = require("../middleware");
 const userRouter = Router();
 
 const signupSchema = zod.object({
@@ -41,6 +42,88 @@ userRouter.post("/signup", async (req, res, next) => {
     res.status(200).json({
         message: "Signup success fully",
         token: token
+    })
+})
+
+userRouter.post("/signin", async (req, res) => {
+    const { safeParse } = signupSchema.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            message: "Emaily is already taken try new things"
+        })
+    }
+
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    if(user){
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRETE)
+
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    return res.json({
+        message: "Error while loginin in"
+    })
+})
+
+
+const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional()
+})
+
+
+userRouter.put("/", authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(user.body);
+    if(!success){
+        return res.status(411).json({
+            message: "Emaily is already taken try new things"
+        })
+    }
+
+    await User.updateOne(req.body, {
+        id: req.userId
+    })
+
+    res.json({
+        message: "Updated Succefully"
+    })
+})
+
+//select * from user where ename is like = '%shra%'
+// Similarly if i want to do this operation in MongoDB then follow the below syntax
+
+userRouter.get("/bulk", async(req, res) => {
+    const filter = req.query.filter || '';
+    
+    const users = await User.find({
+        $or:[{
+                firstName: {
+                    "$regex": filter
+                }
+            },{
+                lastName: {
+                    "$regex": filter
+                }
+            }
+        ]
+    }) 
+
+    res.json({
+        user: users.map(user => ({
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName
+        }))
     })
 })
 
